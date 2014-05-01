6e3de2d529440f524aa4c30d08698337334c5852
Eben <eben@ebenshapiro.com>
2014-05-01 14:45:10 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once('ebenCalendar.php');
+?><!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/ebenCalendar.css">
+</head>
+<body>
+<?php
+  $basicCalendar = new EbenCalendar();
+  $basicCalendar->draw();
+
+?>
+
+
+</body>
+</html>
